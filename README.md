3e6507c4f983bfbda5c00d6bd58b46a161d9c285
Jefferson Matheus <jefferson227@gmail.com>
2019-12-18 23:38:37 -0300

    init

--- README.md
+++ README.md
@@ -1,21 +1,3 @@
-# busca-cinema-backend
+# nodejs-template
 
-This is the repository dedicated to be the backend to the [busca-cinema](https://github.com/Jefferson227/busca-cinema) project. Here, I give some shots on [NodeJS](https://nodejs.org/download/) with [Express](https://expressjs.com/en/starter/installing.html) and even some web scrapping with [CheerioJS](https://cheerio.js.org/).
-
-## Location
-
-There are different branches for specific locations:
-* `master` for Brazilian cities
-* `feature/tribute` for Canadian cities
-
-## Setup
-
-Install the dependencies
-```sh
-$ npm i
-```
-
-Run
-```sh
-$ node index.js
-```
+Just a small boilerplate to create Nodejs projects.
--- index.js
+++ index.js
@@ -1,72 +1,21 @@
-var express = require('express');
-var cheerio = require('cheerio');
-var request = require('request');
+var express = require("express");
 var app = express();
-var baseUrl = 'https://api-content.ingresso.com/v0';
-var partnership = '/partnership/0';
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000; // Useful for server configuration like Heroku
 
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+// Dealing with the CORS issue
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
   return next();
 });
 
-app.get('/', function (req, res) {
-  res.send('Hello this is the busca-cinema-backend :)');
+app.get("/", function(req, res) {
+  res.send("Hello world :)");
 });
 
-app.get('/events/city/:id', function (req, res) {
-  var url = baseUrl + '/events/city/' + req.params.id + partnership;
-
-  request(url, function (error, response, html) {
-    if (!error) {
-      res.send(html || []);
-    }
-    else {
-      res.send(error);
-    }
-  });
+app.get("/example/:id", function(req, res) {
+  res.send(`Hello ${req.params.id}`);
 });
 
-app.get('/events/:id', function (req, res) {
-  var url = baseUrl + '/events/' + req.params.id + partnership;
-
-  request(url, function (error, response, html) {
-    if (!error) {
-      res.send(html || []);
-    }
-    else {
-      res.send(error);
-    }
-  });
-});
-
-app.get('/events/:id', function (req, res) {
-  var url = baseUrl + '/events/' + req.params.id + partnership;
-
-  request(url, function (error, response, html) {
-    if (!error) {
-      res.send(html || []);
-    }
-    else {
-      res.send(error);
-    }
-  });
-});
-
-app.get('/sessions/city/:cityId/event/:movieId/date/:date', function (req, res) {
-  var url = baseUrl + '/sessions/city/' + req.params.cityId + '/event/' + req.params.movieId + partnership + '?date=' + req.params.date;
-
-  request(url, function (error, response, html) {
-    if (!error) {
-      res.send(html || []);
-    }
-    else {
-      res.send(error);
-    }
-  });
-});
-
-app.listen(port, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function() {
+  console.log("Example app listening on port 3000!");
 });
